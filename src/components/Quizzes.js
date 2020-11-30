a3d35c9a40bdd453847670739a3746834fd8ccbb
tianxiangzhu <70231867+tianxiangzhu@users.noreply.github.com>
2020-11-29 17:02:36 -0700

    Create Quizzes.js

---
+++ src/components/Quizzes.js
@@ -0,0 +1,31 @@
+/*var quizzes = [
+    ['Quiz1',"daffodil.png",
+        [
+            ["apple.png",["banana","apple","watermelon","dog"],"apple"],
+            ["dog.jpeg",["cat","car","whale","dog"],"dog"],
+            ["cat.jpg",["cat","car","whale","dog"],"cat"],
+            ["car.jpeg",["cat","car","whale","dog"],"car"],
+            ["whale.jpg",["cat","car","whale","dog"],"whale"],
+            ["fish.jpeg",["cat","fish","whale","dog"],"fish"],
+        ]],
+    ['Quiz2','cherryblossom.png',
+        [
+            ["pie.jpeg",["Banana","pie","watermelon","dog"],"pie"],
+            ["pancakes.jpeg",["cat","pancakes","whale","dog"],"pancakes"],
+            ["cat.jpg",["cat","car","whale","dog"],"cat"],
+            ["car.jpeg",["cat","car","whale","dog"],"car"],
+            ["whale.jpg",["cat","car","whale","dog"],"whale"],
+            ["fish.jpeg",["cat","fish","whale","dog"],"fish"],
+        ]],
+    ['Quiz3','daisy.jpg',
+        [
+            ["pie.jpeg",["Banana","pie","watermelon","dog"],"pie"],
+            ["pancakes.jpeg",["cat","pancakes","whale","dog"],"pancakes"],
+            ["cat.jpg",["cat","car","whale","dog"],"cat"],
+            ["car.jpeg",["cat","car","whale","dog"],"car"],
+            ["whale.jpg",["cat","car","whale","dog"],"whale"],
+            ["fish.jpeg",["cat","fish","whale","dog"],"fish"],
+        ]]
+]
+export default quizzes;
+*/

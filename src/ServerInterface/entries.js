c3278794f32ca1bd5d10ce9e639a4279b12f84bf
tianxiangzhu <70231867+tianxiangzhu@users.noreply.github.com>
2020-11-29 17:03:55 -0700

    Create entries.js

---
+++ src/ServerInterface/entries.js
@@ -0,0 +1,20 @@
+let entries = [
+    {name: 'hanker',
+    origin: '(English) probably related to hang.',
+    example:['when a man has his proper station in life, he does not'],
+    meanings: [
+        {type: "eng",
+        meaning:"this is meaning"}
+    ]},
+
+    {name: 'game',
+    origin: '(English) important for me.',
+    example: ['fps tps rpg'],
+    meanings: [
+        {type: "chi",
+        meaning:"this is another meaning"}
+    ]
+}
+]
+
+export default entries;
